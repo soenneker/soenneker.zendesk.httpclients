8a24aa2da87e53fabec5295364351fb6c7c02b6a
soenneker <jake@soenneker.com>
2026-08-29 14:17:29 -0500

    Improve public API XML documentation

--- src/Soenneker.Zendesk.HttpClients/Abstract/IZendeskOpenApiHttpClient.cs
+++ src/Soenneker.Zendesk.HttpClients/Abstract/IZendeskOpenApiHttpClient.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Zendesk.HttpClients.Abstract;
 /// </summary>
 public interface IZendeskOpenApiHttpClient: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Returns the configured HTTP client used by the Zendesk OpenAPI HTTP Client.
+    /// </summary>
+    /// <param name="cancellationToken">Token used to cancel the operation.</param>
+    /// <returns>A task whose result is the requested HTTP client.</returns>
     ValueTask<HttpClient> Get(CancellationToken cancellationToken = default);
 }
--- src/Soenneker.Zendesk.HttpClients/Registrars/ZendeskOpenApiHttpClientRegistrar.cs
+++ src/Soenneker.Zendesk.HttpClients/Registrars/ZendeskOpenApiHttpClientRegistrar.cs
@@ -13,6 +13,8 @@ public static class ZendeskOpenApiHttpClientRegistrar
     /// <summary>
     /// Adds <see cref="ZendeskOpenApiHttpClient"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddZendeskOpenApiHttpClientAsSingleton(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton()
@@ -24,6 +26,8 @@ public static class ZendeskOpenApiHttpClientRegistrar
     /// <summary>
     /// Adds <see cref="ZendeskOpenApiHttpClient"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddZendeskOpenApiHttpClientAsScoped(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton()
