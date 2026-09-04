af14498f162cba6ba0d04c3ca2d7c7d08a32d32e
soenneker <jake@soenneker.com>
2026-09-03 22:02:49 -0500

    Add interface inheritdoc references

--- src/Soenneker.Zendesk.HttpClients/ZendeskOpenApiHttpClient.cs
+++ src/Soenneker.Zendesk.HttpClients/ZendeskOpenApiHttpClient.cs
@@ -11,6 +11,7 @@ using Soenneker.Utils.HttpClientCache.Abstract;
 
 namespace Soenneker.Zendesk.HttpClients;
 
+/// <inheritdoc cref="IZendeskOpenApiHttpClient" />
 public sealed class ZendeskOpenApiHttpClient : IZendeskOpenApiHttpClient
 {
     private readonly IHttpClientCache _httpClientCache;
